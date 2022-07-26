a427c1a1d077e486ce436bf61a55e12d9c0ad4e1
heloobaby <sun888486@qq.com>
2022-07-26 17:21:08 +0800

    add CPLUSPLUS macro

--- khook/khook/hk.h
+++ khook/khook/hk.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <ntddk.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 _IRQL_requires_max_(APC_LEVEL)
 NTSTATUS HkDetourFunction(
 	_In_ PVOID	 TargetFunction,
@@ -13,3 +17,8 @@ NTSTATUS HkRestoreFunction(
 	_In_ PVOID	 HookedFunction,
 	_In_ PVOID	 OriginalTrampoline
 );
+
+
+#ifdef __cplusplus
+}
+#endif
